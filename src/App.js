0d14b04ba2596e04677a7a43197836114b9aa140
carlosanguix <carlos.anguix@gmail.com>
2021-03-16 12:07:37 +0100

    Redux, signIn page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Login from './components/login/';
+import SignIn from './components/SignIn';
 
 function App() {
   
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/login" exact>
-          <Login />
+        <Route path="/signIn" exact>
+          <SignIn />
         </Route>
       </Switch>
     </BrowserRouter>
